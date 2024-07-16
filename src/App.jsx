a454c88ddb2e9110fbee4aa794a1fd78dfca1046
yelantsevv <yelantsevv@gmail.com>
2024-07-16 10:16:39 +0300

    abra cadabra

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import clsx from "clsx";
 
 function App() {
   const [boxCl, setBoxCl] = useState("box3");
@@ -8,15 +9,15 @@ function App() {
     setBoxCl("box3 box3_wrapper");
     setTimeout(() => {
       setBoxCl("box3");
-    }, 5500);
+    }, 10000);
   };
 
   return (
     <div
-      className={boxCl != "box3" ? "box3 box3_wrapper" : "box3_wrapper"}
+      className={clsx("box3_wrapper", { box3: boxCl !== "box3" })}
       onMouseEnter={hand}
     >
-      <p className="box3_title">Article Time</p>
+      Article Time
     </div>
   );
 }
